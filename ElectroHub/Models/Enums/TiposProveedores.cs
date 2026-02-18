b47a2bc80eb443847d58274c63aac7b472aa5c59
JOEL ESCANO GUZMAN <joel_escano@ucne.edu.do>
2026-02-17 23:03:20 -0400

    Crea Enum de TiposProveedores

---
+++ ElectroHub/Models/Enums/TiposProveedores.cs
@@ -0,0 +1,12 @@
+﻿namespace ElectroHub.Models.Enums
+{
+    public enum TiposProveedores
+    {
+        ProveedorMayorista,
+        ProveedorMinorista,
+        ProveedorImportador,
+        ProveedorLocal,
+        ProveedorServicios,
+        ProveedorFabricante
+    }
+}
