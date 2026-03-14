d6f579c6085ebeb4b7b007a4543a0e35856d3622
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-03-14 16:34:11 -0400

    Last Update

--- ElectroHub/Services/ProveedoresService.cs
+++ ElectroHub/Services/ProveedoresService.cs
@@ -43,6 +43,15 @@ public class ProveedoresService(IDbContextFactory<ApplicationDbContext> DbContex
         return await context.Proveedores.FirstOrDefaultAsync(proveedor => proveedor.ProveedorId == Id);
     }
 
+    public async Task<bool> Recuperar(int proveedorId)
+    {
+        await using var contexto = await DbContextFactory.CreateDbContextAsync();
+        var filasAfectadas = await contexto.Proveedores!
+            .Where(p => p.ProveedorId == proveedorId)
+            .ExecuteUpdateAsync(p =>
+                p.SetProperty(x => x.Eliminado, false));
+        return filasAfectadas > 0;
+    }
     public async Task<bool> Eliminar(int Id)
     {
         await using var context = await DbContextFactory.CreateDbContextAsync();
